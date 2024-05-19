cb66d84199eeb04c3d81700338944b4d48186a1b
fleury_mathis <mathis@lanageuse.com>
2024-05-20 00:07:06 +0200

    add

--- src/components/site/InfoSiteAuth.jsx
+++ src/components/site/InfoSiteAuth.jsx
@@ -1,17 +1,22 @@
 export default function InfoSiteAuth() {
-    return (
-      <div className="h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <p className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Get the help you need</p>
-            <h2 className="mt-2 text-4xl font-bold tracking-tight text-slate-100 sm:text-6xl">Support center</h2>
-            <p className="mt-6 text-lg leading-8 text-slate-300">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
-            </p>
-          </div>
+  return (
+    <div className="h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <p className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+            GetHead
+          </p>
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-slate-100 sm:text-6xl">
+            Créez votre compte
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-slate-300">
+            Créez facilement des API de test personnalisées avec des données
+            fictives en quelques clics, grâce à notre générateur d'API alimenté
+            par Faker.js. Simplifiez votre processus de développement et
+            améliorez la qualité de vos tests avec nos solutions sur mesure.
+          </p>
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
